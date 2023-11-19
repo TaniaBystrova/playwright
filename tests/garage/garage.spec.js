4de8725ac6e75b2b97d3344e14c34adba48cbaba
Tania <bystrovat@procoders.tech>
2023-11-19 19:00:14 +0200

    removed extra import

--- tests/garage/garage.spec.js
+++ tests/garage/garage.spec.js
@@ -1,5 +1,4 @@
-import {test} from '../../src/fixtures/userGaragePage.fixture.js'
-const test = require('../../src/fixtures/userGaragePage.fixture.js')
+import {test} from '../../src/fixtures/userGaragePage.fixture.js';
 import {expect} from "@playwright/test";
 import ProfilePage from "../../src/pageObjects/profilePage/ProfilePage.js";
 import GaragePage from "../../src/pageObjects/panel/garagePage/GaragePage.js";
@@ -7,6 +6,7 @@ import GaragePage from "../../src/pageObjects/panel/garagePage/GaragePage.js";
 test.describe('User signs in', ()=>{
      test('should use storage state', async ({page, userInfo})=>{
         const garagePage = new GaragePage(page)
+        const profilePage = new ProfilePage(page)
          await profilePage.navigate()
         await expect(profilePage.userName, "valid user name should be displayed").toHaveText(`${userInfo.name} ${userInfo.lastName}`)
      })
